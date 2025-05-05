283eeec96a295a2e74511842bd529c844d691a9e
khairy hassan <khairy mawed@gmail.com>
2025-05-05 21:22:35 +0100

    testimonials section

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ menuOpenButton.addEventListener("click", () => {
 
 // close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor:true,
+  spaceBetween:25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // responsive beakpoints
+  breakpoints: {
+    0: {
+      slidesPerView:1
+    },
+    768: {
+      slidesPerView:2
+    },
+    1024: {
+      slidesPerView:3
+    },
+  }
+});
